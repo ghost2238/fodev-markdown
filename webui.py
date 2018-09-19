07e030349b7a1d14178e180e1ff93659e50ca60f
Markus <ghsthack@gmail.com>
2018-09-19 21:49:04 +0200

    Seems to be working

--- webui.py
+++ webui.py
@@ -15,29 +15,53 @@ def get_projects():
     return content
 
 @app.route("/<project_name>")
-@app.route("/<project_name>/<file>")
-def show_table(project_name, file=''):    
+@app.route("/<project_name>/<page_route>")
+@app.route("/<project_name>/<page_route>/<doc_route>")
+def show_table(project_name, page_route='', doc_route=''):    
     projects = get_projects()
 
     if project_name not in projects:
         return render_template('select.html', )
 
     project = projects[project_name]
-    print(projects[project_name])
-    if file is '':
-        file = project['items'][0]['filename']
+    if page_route is '':
+        page = project['pages'][0]
+    else:
+        page = project['pages'][page_route]
+    
+    docs = page['docs']
+    if doc_route is '':
+        # Take first document on page.
+        doc = docs[0]
+        doc_route = doc['route']
+    else:
+        for x in range(0,len(docs)):
+            if doc_route == docs[x]['route']:
+                doc = docs[x]
 
-    sanitized_file = file.replace('..','')
-    if 'github_repo' in project:
-        with open(config.md_path+project['route_name']+'/'+sanitized_file, 'r', ) as fp:
+    url = doc['url']
+    url = url.replace('..','')
+    github_repo = ''
+    protocol = 'file'
+    if url.find('://') != -1:
+        spl = url.split('://')
+        if spl[0] == 'github':
+            protocol = 'github'
+            last_slash = spl[1].rfind('/')
+            github_repo = spl[1][:last_slash] 
+            url = spl[1][last_slash:]
+
+    if protocol == 'file':
+        with open(config.md_path+project['route_name']+'/'+url, 'r', ) as fp:
             content = ''.join(fp.readlines())
     else:
-        url = 'https://raw.githubusercontent.com/'+project.github_repo+'/master/'+sanitized_file
-        r = requests.get(url)
+        request_url = 'https://raw.githubusercontent.com/'+github_repo+'/master/'+url
+        r = requests.get(request_url)
         content = r.text
 
     html = markdown.markdown(content, extensions=[GithubFlavoredMarkdownExtension()])
-    return render_template('index.html', base_href=config.base_href, file=sanitized_file, html_content=html, project=project)
+    return render_template('index.html', base_href=config.base_href, 
+        selected_page=page, page_route=page_route, doc_route=doc_route, docs=docs, url=url, html_content=html, project=project)
 
 @app.route("/")
 def show_index():
